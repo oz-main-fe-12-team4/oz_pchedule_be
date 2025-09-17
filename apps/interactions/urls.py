ef2d08d0372107ac9f819f24cdb3dbfd907f5001
DESKTOP-NCSJNG2\Me <zkzktldk1234@gmail.com>
2025-09-17 18:45:28 +0900

    🐛 Fix code interactions urls, notification views
    fix code interactions urls, notification views

--- apps/interactions/urls.py
+++ apps/interactions/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from apps.interactions.views import PostFavoriteAPIView, PostLikeAPIView, PostReportAPIView
 
 urlpatterns = [
-    path("post/{post_id}/like", PostLikeAPIView.as_view(), name="transactions-like"),
-    path("post/{post_id}/favorite", PostFavoriteAPIView.as_view(), name="transactions-favorite"),
-    path("post/{post_id}/report", PostReportAPIView.as_view(), name="transactions-report"),
+    path("post/<int:pk>/like", PostLikeAPIView.as_view(), name="transactions-like"),
+    path("post/<int:pk>/favorite", PostFavoriteAPIView.as_view(), name="transactions-favorite"),
+    path("post/<int:pk>/report", PostReportAPIView.as_view(), name="transactions-report"),
 ]
