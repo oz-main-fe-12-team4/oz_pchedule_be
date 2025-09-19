d6711f3d43f4494a971fe7e56d5e4bcb04d6bfb4
DESKTOP-NCSJNG2\Me <zkzktldk1234@gmail.com>
2025-09-19 11:27:00 +0900

    🐛 Fix interactions models
    fix interactions models

--- apps/interactions/models.py
+++ apps/interactions/models.py
@@ -4,7 +4,7 @@ from apps.post.models import Post
 
 
 class Like(models.Model):
-    like_id = models.IntegerField(primary_key=True, auto_created=True)
+    like_id = models.CompositePrimaryKey("user", "post")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
@@ -15,32 +15,24 @@ class Like(models.Model):
         return f"like ID = {self.like_id}"
 
 
-class Favorite(models.Model):
-    favorite_id = models.IntegerField(primary_key=True, auto_created=True)
+class Bookmark(models.Model):
+    bookmark_id = models.CompositePrimaryKey("user", "post")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
     class Meta:
-        db_table = "favorite"
+        db_table = "bookmark"
 
     def __str__(self):
-        return f"favorite ID = {self.favorite_id}"
+        return f"bookmark ID = {self.bookmark_id}"
 
 
 class Report(models.Model):
-    REASON = [
-        ("Inappropriate name", "부적절한 이름"),
-        ("Inappropriate content", "부적절한 내용"),
-        ("Spamming", "도배"),
-        ("Profanity", "욕설"),
-        ("Sexually explicit content", "선정적인 내용"),
-    ]
-
     report_id = models.IntegerField(primary_key=True, auto_created=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    reason = models.CharField(max_length=50, choices=REASON)
-    created_at = models.DateTimeField(auto_now_add=True)
+    reason = models.CharField(max_length=50, null=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
 
     class Meta:
         db_table = "report"
