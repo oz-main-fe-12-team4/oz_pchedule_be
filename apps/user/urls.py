8dfcb0ccbea15662c509ea93d40f04bda05106b3
CHE1210"clearclaer;gitzzzzzdkdkkdf <didlal0902@gmail.com>
2025-09-22 14:26:30 +0900

    user 수정5

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import (
     SignupView,
     LoginView,
-    SocialLoginView,
     LogoutView,
     UserInfoView,
-    UserEditView,
+    UserNameEditView,
+    UserPasswordEditView,
     UserDeleteView,
     TokenRefreshView,
 )
@@ -13,10 +13,10 @@ from .views import (
 urlpatterns = [
     path("signup", SignupView.as_view(), name="signup"),
     path("login", LoginView.as_view(), name="login"),
-    path("social-login", SocialLoginView.as_view(), name="social-login"),
     path("logout", LogoutView.as_view(), name="logout"),
     path("me", UserInfoView.as_view(), name="user-info"),
-    path("me/edit", UserEditView.as_view(), name="user-edit"),
+    path("me/edit/name", UserNameEditView.as_view(), name="user-edit-name"),
+    path("me/edit/password", UserPasswordEditView.as_view(), name="user-edit-password"),
     path("me/withdraw", UserDeleteView.as_view(), name="user-delete"),
     path("token/refresh", TokenRefreshView.as_view(), name="token-refresh"),
 ]
