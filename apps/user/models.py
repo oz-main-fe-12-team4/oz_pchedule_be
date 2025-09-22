8dfb4b489f49433b7e7b7be6e8d7f75b559247a9
CHE1210"clearclaer;gitzzzzzdkdkkdf <didlal0902@gmail.com>
2025-09-22 14:59:43 +0900

    user 수정6

--- apps/user/models.py
+++ apps/user/models.py
@@ -14,7 +14,6 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser):
-    user_id = models.BigAutoField(primary_key=True)  # PK, AUTO_INCREMENT
     email = models.EmailField(max_length=255, unique=True, null=False, blank=False)
     password = models.CharField(max_length=255, null=False, blank=False)
     name = models.CharField(max_length=20, unique=True, null=False, blank=False)
