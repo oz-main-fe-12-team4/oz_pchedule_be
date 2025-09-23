52ceb8dea7d1d9985fbc48da1d7ea9f8a5e5fbcf
junho <junho@aui-MacBookPro.local>
2025-09-23 15:43:13 +0900

    코드다시수정

--- apps/schedule/urls.py
+++ apps/schedule/urls.py
@@ -1,7 +1,15 @@
+# apps/schedule/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, ScheduleViewSet, DetailScheduleViewSet, RecurrenceViewSet, WeekdayViewSet
+from .views import (
+    CategoryViewSet,
+    WeekdayViewSet,
+    ScheduleViewSet,
+    DetailScheduleViewSet,
+    RecurrenceViewSet,
+)
 
+# DRF DefaultRouter 생성
 router = DefaultRouter()
 router.register("categories", CategoryViewSet)
 router.register("weekdays", WeekdayViewSet)
@@ -9,6 +17,7 @@ router.register("schedules", ScheduleViewSet)
 router.register("details", DetailScheduleViewSet)
 router.register("recurrences", RecurrenceViewSet)
 
+# URL 패턴 등록
 urlpatterns = [
     path("", include(router.urls)),
 ]
