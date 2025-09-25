1fb4b9f5be7fb09f26d6c56a44bcc8a01c598703
DESKTOP-NCSJNG2\Me <zkzktldk1234@gmail.com>
2025-09-25 14:44:40 +0900

    🐛 Fix settings -> base, dev, prod / docker-compose.yml for prod.
    Fix settings -> base, dev, prod / docker-compose.yml for prod.

---
+++ config/settings/prod.py
@@ -0,0 +1,19 @@
+from .base import *
+import os
+
+DEBUG = False
+
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
+
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
