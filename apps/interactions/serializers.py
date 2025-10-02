354d2bc358e10e131b17c2cbbffde27e856f6c94
CHE1210"clearclaer;gitzzzzzdkdkkdf <didlal0902@gmail.com>
2025-10-02 13:30:55 +0900

    interactions serializers 수정

--- apps/interactions/serializers.py
+++ apps/interactions/serializers.py
@@ -3,9 +3,7 @@ from .models import Report
 
 
 class ReportSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
     reason = serializers.ChoiceField(choices=Report.REASONS)
-    created_at = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = Report
